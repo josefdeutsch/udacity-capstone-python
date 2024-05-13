5626ee4094b7d9ff2932adaee79b2a25bef40d4e
Joseph <philippdeutsch@icloud.com>
2024-05-13 19:35:08 +0000

    python3-starter-setup

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 from setuptools import setup, find_packages
 
 setup(
-    name='example',
-    version='0.1dev0',
-    author='Author Name', 
-    author_email='author_email@mail.com',
+    name='Udacity-capstone-python',
+    version='0.1',
+    author='Joseph Deutsch', 
+    author_email='philippdeutsch@icloud.com',
     packages=find_packages(),
     long_description=open('README.md').read()
 )
